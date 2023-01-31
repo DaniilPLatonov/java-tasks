2f3b982c247c44460f28f08d44a72be3be2023ad
DaniilPlatonov <d_platonov@internet.ru>
2023-01-31 20:11:36 +0300

    Initial Commit

--- src/JetBrains_tasks/OutputStream.java
+++ src/JetBrains_tasks/OutputStream.java
@@ -1,20 +1,17 @@
 package JetBrains_tasks;
 
-import java.io.ByteArrayOutputStream;
-import java.io.CharArrayWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+interface Foo{ int x = 10;}
 
 public class OutputStream {
-    public static void main(String[] args) {
-        String [] str = new String []{"This", " ", "is", " ", "a", " ", "test"};
-        try {
-            System.out.println(Arrays.toString(Converter.convert(str)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+
     }
 }
 
@@ -22,9 +19,11 @@ class Converter {
     public static char[] convert(String[] words) throws IOException {
 
         CharArrayWriter contactWriter = new CharArrayWriter();
-     contactWriter.write(String.valueOf(words));
-     char [] array = contactWriter.toCharArray();
-     contactWriter.close();
-     return array;
+        for (int i = 0; i < words.length; i++) {
+            contactWriter.write(words[i]);
+        }
+        char[] array = contactWriter.toCharArray();
+        contactWriter.close();
+        return array;
     }
 }
