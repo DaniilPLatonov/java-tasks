b66b317311bdf35738188363c24bbafcbc36d4b3
DaniilPlatonov <d_platonov@internet.ru>
2023-02-06 21:51:03 +0300

    Initial Commit

--- src/JetBrains_tasks/Book.java
+++ src/JetBrains_tasks/Book.java
@@ -1,10 +1,30 @@
 package JetBrains_tasks;
 
 
+
+
+import java.util.Scanner;
+
+record Person(String name, int age, String gender) {
+    //don't change this record
+    public String name() {
+        return name;
+    }
+    public int age() {
+        return age;
+    }
+    public String gender() {
+        return gender;
+    }
+
+}
+
+
 class Main {
     public static void main(String[] args) {
-        Book book = new Book("a",5, new String[]{"4","d"});
-        System.out.println(book.toString());
+        Scanner scanner = new Scanner(System.in);
+        Person person = new Person(scanner.next(), scanner.nextInt(), scanner.next());
+        System.out.println(person.name() + " is " + person.age() + " years old and is " + person.gender() );
     }
 
 }
@@ -23,7 +43,35 @@ class Book {
 
     @Override
     public String toString() {
-        return "title=" + title + ",yearOfPublishing=" + yearOfPublishing + ",authors=" + java.util.Arrays.toString(this.authors).replace(", ", ",");
+        return "title=" + title + " yearOfPublishing=" + yearOfPublishing + ",authors=" + java.util.Arrays.toString(this.authors).replace(", ", ",");
     }
 }
 
+class Vehicle {
+
+    protected String licensePlate;
+
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{licensePlate=" + licensePlate + "}";
+    }
+}
+
+class Car extends Vehicle {
+
+    protected int numberOfSeats;
+
+    public Car(String licensePlate, int numberOfSeats) {
+        super(licensePlate);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{licensePlate=" + licensePlate + ",numberOfSeas=" + numberOfSeats + "}";
+    }
+}
