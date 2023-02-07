0979da0d0a5015660ce4110795b05f8d9eae4b67
DaniilPlatonov <d_platonov@internet.ru>
2023-02-07 14:18:20 +0300

    Initial Commit

--- src/CodeWars_tasks/a.java
+++ src/CodeWars_tasks/a.java
@@ -1,7 +1,20 @@
 package CodeWars_tasks;
 
-public class a {
-    public static void main(String[] args) {
+import java.util.*;
 
+ class SmileFaces {
+
+    public static int countSmileys(List<String> arr) {
+        String[] arr2 = arr.toArray(new String[0]);
+        int count = 0;
+        String[] arr3 = new String[]{":)", ":D", ":-)", ":~)", ":-D", ":~D", ";)", ";D", ";-)", ";~)", ";-D", ";~D"};
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr3.length; j++) {
+                if(arr2[i].equals(arr3[j])){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
