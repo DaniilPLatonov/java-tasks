6b6ed9e693f08f74b5acf29b7e85dcade98d37ff
DaniilPlatonov <d_platonov@internet.ru>
2023-02-19 16:53:48 +0300

    Initial Commit

---
+++ src/LeetCode_tasks/Permutations.java
@@ -0,0 +1,18 @@
+package LeetCode_tasks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Permutations {
+
+    public List<List<Integer>> permute(int[] nums) {
+         List<Integer> a = new ArrayList<>();
+         return Collections.singletonList(a);
+    }
+
+    public static void main(String[] args) {
+        int b [] = new int[]{1,2,3};
+    }
+
+}
