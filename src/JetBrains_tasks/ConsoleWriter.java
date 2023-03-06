97667c2c84936bd4782cc6ccf196ec42ab7ec43a
DaniilPlatonov <d_platonov@internet.ru>
2023-03-06 18:52:13 +0300

    Initial Commit

---
+++ src/JetBrains_tasks/ConsoleWriter.java
@@ -0,0 +1,21 @@
+package JetBrains_tasks;
+
+class ConsoleWriter implements Printer, Notifier {
+    @Override
+    public void greeting() {
+        Printer.super.greeting();
+    }
+    // override greeting method according to Printer default implementation
+}
+
+interface Printer {
+    default void greeting() {
+        System.out.println("Printer is ready");
+    }
+}
+
+interface Notifier {
+    default void greeting() {
+        System.out.println("Notifier is ready");
+    }
+}
