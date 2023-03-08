dcf0d54d767e2782aec1522d9ebf02b47dd03e97
DaniilPlatonov <d_platonov@internet.ru>
2023-03-08 15:32:29 +0300

    Initial Commit

---
+++ src/LeetCode_tasks/Matrix.java
@@ -0,0 +1,21 @@
+package LeetCode_tasks;
+
+public class Matrix {
+    public static void main(String[] args) {
+        int a[][] = new int[][]{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        System.out.println();
+    }
+
+/*
+    public static int[][] updateMatrix(int[][] mat) {
+        
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+
+            }
+        }
+    }
+*/
+
+
+}
