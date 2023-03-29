4963a203ad30e61fd996dde3a7aece8b31f230e9
DaniilPlatonov <d_platonov@internet.ru>
2023-03-29 21:08:44 +0300

    Initial Commit

---
+++ src/Udemy_tasks/annotation/Test.java
@@ -0,0 +1,9 @@
+package Udemy_tasks.annotation;
+
+//@MethodInfo(purpose ="Print")
+public class Test {
+    @MethodInfo(purpose = "Print Hello World")
+    public void print(){
+        System.out.println("Heloo world!");
+    }
+}
