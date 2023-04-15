1f422a16dddb7434671573e8ecaddd8d5177bb08
DaniilPlatonov <d_platonov@internet.ru>
2023-04-15 22:58:16 +0300

    Initial Commit

---
+++ src/JetBrains_tasks/LocalDate2.java
@@ -0,0 +1,13 @@
+package JetBrains_tasks;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class LocalDate2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
