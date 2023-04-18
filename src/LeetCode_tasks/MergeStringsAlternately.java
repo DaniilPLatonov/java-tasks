33d98e98b0397d6cf49a99b19c55df9a14a302ff
DaniilPlatonov <d_platonov@internet.ru>
2023-04-18 22:27:56 +0300

    Initial Commit

---
+++ src/LeetCode_tasks/MergeStringsAlternately.java
@@ -0,0 +1,20 @@
+package LeetCode_tasks;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abcd", "pq"));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int n = word1.length();
+        int m = word2.length();
+        int i = 0;
+        while (i < n && i < m){
+            sb.append(word1.charAt(i)).append(word2.charAt(i));
+            i++;
+        }
+        sb.append(word1.substring(i,n)).append(word2.substring(i,m));
+        return sb.toString();
+    }
+}
