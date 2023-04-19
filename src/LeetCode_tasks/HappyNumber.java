77b213962f8dd78fa5838e97329be811c058a18e
DaniilPlatonov <d_platonov@internet.ru>
2023-04-19 22:43:04 +0300

    Initial Commit

---
+++ src/LeetCode_tasks/HappyNumber.java
@@ -0,0 +1,25 @@
+package LeetCode_tasks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(2));
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
