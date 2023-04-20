3256b6418d55fcc33a0baf4be726452d8952adcf
DaniilPlatonov <d_platonov@internet.ru>
2023-04-20 15:39:02 +0300

    Initial Commit

--- src/LeetCode_tasks/SingleNumber.java
+++ src/LeetCode_tasks/SingleNumber.java
@@ -1,20 +1,28 @@
 package LeetCode_tasks;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SingleNumber {
     public static int singleNumber(int[] nums) {
-        int ans=0;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            ans ^= num;
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
         }
-        return ans;
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) return key;
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
-        int array[] = new int[]{4, 1, 5, 5, 1, 2};
+        int array[] = new int[]{2, 2, 3, 2};
         System.out.println(singleNumber(array));
 
     }
