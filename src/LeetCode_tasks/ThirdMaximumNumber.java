c30bd34c21d0deb69cd7124835f2230fe21d3b22
DaniilPlatonov <d_platonov@internet.ru>
2023-04-23 21:30:14 +0300

    Initial Commit

---
+++ src/LeetCode_tasks/ThirdMaximumNumber.java
@@ -0,0 +1,21 @@
+package LeetCode_tasks;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public static int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+        }
+    }
+}
